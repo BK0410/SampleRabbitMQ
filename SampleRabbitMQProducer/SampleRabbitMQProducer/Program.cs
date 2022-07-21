61303d5154d93cd2b05e38be0407b57b582b02e5
BARATHKUMAR M <101806993+BK0410@users.noreply.github.com>
2022-07-21 13:34:46 +0530

    Add files via upload

---
+++ SampleRabbitMQProducer/SampleRabbitMQProducer/Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare("demo-queue",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments:null);
+
+            var message = new { Name = "Producer", message = "Hello World" };
+            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+            channel.BasicPublish("", "demo-queue", null, body);
+
+                }
+    }
+}
